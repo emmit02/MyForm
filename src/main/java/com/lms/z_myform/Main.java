df34368164e6399f2176c44aca6def3a6b9e6e9c
Sanjeevani <sanjeevanisharma741390pal@gmail.com>
2024-01-13 14:02:35 +0530

    initial commit

---
+++ src/main/java/com/lms/z_myform/Main.java
@@ -0,0 +1,14 @@
+package com.lms.z_myform;
+
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) 
+    {
+        System.out.println("Testing..");
+        MyWindow w = new MyWindow();
+    }
+    
+}
---
+++ src/main/java/com/lms/z_myform/Z_MyForm.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.lms.z_myform;
+
+/**
+ *
+ * @author DELL
+ */
+public class Z_MyForm {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
